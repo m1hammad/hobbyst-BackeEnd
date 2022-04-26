e9d5017e82045b616d538c22b533ee91da7fc7bd
Lila <lila.nawar21@gmail.com>
2022-04-26 16:14:47 -0400

    adds events to the hobbies model

--- controllers/event.js
+++ controllers/event.js
@@ -1,22 +1,57 @@
 const {Event} = require('../models/Event') 
 const User = require("../models/User")
 const moment = require('moment')
+const {Hobby} = require('../models/Hobby')
 
-exports.event_post = async (req, res) =>{
+  exports.event_post = async (req, res) =>{
   // need to push the user that is being passed in as a param to the event.users list
     // console.log('hitting controller')
 
     console.log("req.body user is",req.params.userid)
     let newEvent= new Event(req.body)
+    console.log("body", req.body)
 
     newEvent.users.push(req.params.userid)
+    console.log("new event", newEvent)
 
     await newEvent.save()
     let user = await User.findById(req.params.userid)
     user.events.push(newEvent._id)
     user.save()
-    res.status(200).send("Done")
+    let hobbies = await newEvent.hobby
+    hobbies.forEach(async hobbyid => {
+      let hobby = await Hobby.findById(hobbyid._id)
+      hobby.events.push(hobbyid._id)
+      hobby.save()
+      let results = await Hobby.findById(hobbyid._id)
+      console.log(results)
+    })
+    // find the id's of the hobbies in Event
+    // let hobbies = await newEvent.hobby
+    // hobbi =Hobby.find({})
+   
+    // for (let hobby in hobbies){
+    //   await hobbi.findById(hobby._id).push(newEvent._id)
+    //   console.log("hobby", Hobby.findById(hobby._id))
+    //   Hobby.save()
+    // }
+    res.status(200).send('Done')
+  
+  }
 
+
+    // exports.hobby_add_avent = async(req, res) => {
+    //   console.log("email:", req.params.email)
+    //   console.log("req hobby ", req.body)
+    //   let hobbyIds = req.body
+    //   let user = await User.findOne({emailAddress: req.params.email})
+    //   hobbyIds.forEach(async id => {
+    //       let hobby = await Hobby.findById(id)
+    //       hobby.users.push(user._id)
+    //       hobby.save()
+    //   })
+      
+      
   //   .then(async newEvent => {
   //     console.log("evet is", await newEvent)
   //     res.json({newEvent})
@@ -25,5 +60,5 @@ exports.event_post = async (req, res) =>{
   //     console.log(err)
   //     res.send('ERROR')
   // })
-}
+
 
