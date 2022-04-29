03d84e484397dd50f5619f8e16765bf54d645530
Muhammad Hammad Riaz <hammad27r@hotmail.com>
2022-04-29 10:39:45 -0400

    d

--- routes/hobby.js
+++ routes/hobby.js
@@ -1,7 +1,6 @@
 const router = require('express').Router() 
 const hobbyCntrl = require('../controllers/hobby')  
 const methodOverride = require('method-override')
-const { append } = require('express/lib/response')
 
 
 router.use(methodOverride('_method'))
